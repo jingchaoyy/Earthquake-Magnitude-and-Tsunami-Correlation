35be92569c10ecc15d932d273e49d9ebba862968
jingchaoyy <jcyangt@gmail.com>
2018-05-02 15:00:10 -0400

    adding scatter plot
    scatter plot for Tsunami Observation Regarding Earthquake Surface
    Magnitude and Focal Depth

--- plot.py
+++ plot.py
@@ -4,38 +4,28 @@ Created on 4/30/18
 @author: YJccccc
 """
 import dataPrepro
+import dataFilter
 from matplotlib import pyplot as plt
 
 '''Time Series Plot'''
-fig, ax = plt.subplots()
-ax.plot(dataPrepro.timeLine, dataPrepro.equakeList, 'r', label="Earthquake")
-ax.plot(dataPrepro.timeLine, dataPrepro.equakeLocationFilteredList, 'g', label="Earthquake Filtered")
-ax.plot(dataPrepro.timeLine, dataPrepro.tsuList, 'b', label="Tsunami")
-
-legend = ax.legend(loc='upper left', shadow=True, fontsize='large')
-
-plt.title('Time Series')
-plt.xlabel('Year')
-plt.ylabel('Event Counts')
-plt.show()
-
-'''Trend Plot'''
-# decomposition = seasonal_decompose(equakeList)
-# trend = decomposition.trend
-# seasonal = decomposition.seasonal
-# residual = decomposition.resid
+# fig, ax = plt.subplots()
+# ax.plot(dataPrepro.timeLine, dataPrepro.equakeList, 'r', label="Earthquake")
+# ax.plot(dataPrepro.timeLine, dataPrepro.equakeLocationFilteredList, 'g', label="Earthquake Filtered")
+# ax.plot(dataPrepro.timeLine, dataPrepro.tsuList, 'b', label="Tsunami")
 #
-# plt.subplot(411)
-# plt.plot(timeLine)
-# plt.title('Original Time Series')
-# plt.subplot(412)
-# plt.plot(trend)
-# plt.title('Trend')
-# plt.subplot(413)
-# plt.plot(seasonal)
-# plt.title('Seasonality')
-# plt.subplot(414)
-# plt.plot(residual)
-# plt.title('Residuals')
-# plt.tight_layout()
+# legend = ax.legend(loc='upper left', shadow=True, fontsize='large')
+#
+# plt.title('Time Series')
+# plt.xlabel('Year')
+# plt.ylabel('Event Counts')
 # plt.show()
+
+'''Scatter Plot'''
+X = dataFilter.tsuOnly['EQ_MAG_MS']
+Y = dataFilter.tsuOnly['FOCAL_DEPTH']
+
+plt.title('Tsunami Observation Regarding Earthquake Surface Magnitude and Focal Depth')
+plt.scatter(X, Y)
+plt.xlabel("Surface Magnitude")
+plt.ylabel("Focal Depth (km)")
+plt.show()
