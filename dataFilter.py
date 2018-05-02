41d8bbfcd0c647c9ae245dbd3cc6463bc8a7c713
jingchaoyy <jcyangt@gmail.com>
2018-05-02 11:04:14 -0400

    data preprocessing part2
    Applying data filtering, collect only data records that has complete
    surface magnitude and formal depth information with tsunami data
    covered to 0 and 1

--- dataFilter.py
+++ dataFilter.py
@@ -7,6 +7,7 @@ import dataPrepro
 import pandas as pd
 
 allData = dataPrepro.earthquake
+# allData = dataPrepro.earthquakeLocationFilter
 # print(allData)
 
 # Eliminating all records without FOCAL_DEPTH
@@ -21,5 +22,18 @@ surfaceMagnitude = filteredData['EQ_MAG_MS']  # surfaceMagnitude data list
 # print(surfaceMagnitude)
 
 # Collect all observed tsu event in filtered data
-tsu = filteredData[pd.notnull(filteredData['FLAG_TSUNAMI'])]
+# tsu = filteredData[pd.notnull(filteredData['FLAG_TSUNAMI'])]
 # print("Total observed: ",len(tsu))
+
+oriTsu = filteredData['FLAG_TSUNAMI']
+tsu0 = oriTsu.fillna(0)  # fill all NaN with 0, meaning no Tsu
+tsu = tsu0.replace('Tsu',1)  # replacing 'Tsu' to 1, and generate new tsunami data list
+# print(tsu)
+
+# concat all three columns to create a new panda df
+# https://stackoverflow.com/questions/20017236/join-three-pandas-data-frames-into-one
+merge = pd.concat([focalDepth,surfaceMagnitude,tsu],axis=1)
+print(merge)
+
+# create new csv with filtered data, ready for further analysis
+merge.to_csv('Data/filtered.csv', encoding='utf-8', index=False)
