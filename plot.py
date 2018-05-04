d30f544e02361b416ab9fd8f34a3533d2ff81856
jingchaoyy <jcyangt@gmail.com>
2018-05-04 17:10:57 -0400

    plot Autocorrelation Coefficients

--- plot.py
+++ plot.py
@@ -41,33 +41,67 @@ and set x,y to surface magnitude, focal depth
 Plotting Zonal Average, cont number of events
 that happened in a specific latitude zone
 '''
-import zonalAverage
-# pre-process data from zonalAverage
-group1 = zonalAverage.earthquakeGroups
-group2 = zonalAverage.tsuGroups
+# import zonalAverage
+# # pre-process data from zonalAverage
+# group1 = zonalAverage.earthquakeGroups
+# group2 = zonalAverage.tsuGroups
+#
+# earthquakeTup = zonalAverage.getTup(group1)
+# tsuTup = zonalAverage.getTup(group2)
+#
+# # sort the event tuple list by latitude
+# sortedearthquakeTup = sorted(earthquakeTup, key=lambda t: t[0])
+# sortTsuTup = sorted(tsuTup, key=lambda t: t[0])
+#
+# eX, eY = [], []
+# for et in sortedearthquakeTup:
+#     eX.append(et[0])
+#     eY.append(et[1])
+#
+# tX, tY = [], []
+# for tt in sortTsuTup:
+#     tX.append(tt[0])
+#     tY.append(tt[1])
+#
+# fig, ax = plt.subplots()
+# ax.plot(eX, eY, 'r', label="Earthquake Count")
+# ax.plot(tX, tY, 'b', label="Tsunami Count")
+# legend = ax.legend(loc='upper left', shadow=True, fontsize='large')
+# plt.title('Zonal Average')
+# plt.xlabel('Lats')
+# plt.ylabel('Event Counts')
+# plt.show()
 
-earthquakeTup = zonalAverage.getTup(group1)
-tsuTup = zonalAverage.getTup(group2)
+"""Autocorrelation Plot
+Plotting for earthquake surface magnitude
+earthquake forcal depth and tsunami maxWaterHeight"""
+import correlation
+from statsmodels.tsa.stattools import acf
 
-# sort the event tuple list by latitude
-sortedearthquakeTup = sorted(earthquakeTup, key=lambda t: t[0])
-sortTsuTup = sorted(tsuTup, key=lambda t: t[0])
+# lag=N/5
+lags = int(len(correlation.surMagFilter) / 5)
+# adopting autocorrelation function
+acfsurMag = acf(correlation.surMagFilter, nlags=lags)
+# print('AOT autocorrelation coefficients:', acfAOT)
+acffodep = acf(correlation.fodepFilter, nlags=lags)
+# print('ENSO autocorrelation coefficients:', acfENSO)
+acfmaxWaterHeight = acf(correlation.maxWaterHeightFilter, nlags=lags)
+# print('TS autocorrelation coefficients:', acfTS)
 
-eX, eY = [], []
-for et in sortedearthquakeTup:
-    eX.append(et[0])
-    eY.append(et[1])
+lagSeries = list(range(0, lags + 1))
 
-tX, tY = [], []
-for tt in sortTsuTup:
-    tX.append(tt[0])
-    tY.append(tt[1])
-
-fig, ax = plt.subplots()
-ax.plot(eX, eY, 'r', label="Earthquake Count")
-ax.plot(tX, tY, 'b', label="Tsunami Count")
-legend = ax.legend(loc='upper left', shadow=True, fontsize='large')
-plt.title('Zonal Average')
-plt.xlabel('Lats')
-plt.ylabel('Event Counts')
+plt.subplot(311)
+plt.plot(lagSeries, acfsurMag, label='Surface Magnitude')
+plt.legend(loc='best')
+plt.title('Autocorrelation Coefficients')
+plt.subplot(312)
+plt.plot(lagSeries, acffodep, label='Focal Depth')
+plt.legend(loc='best')
+plt.subplot(313)
+plt.plot(lagSeries, acfmaxWaterHeight, label='Max Water Height')
+plt.legend(loc='best')
+plt.xlabel('Lag')
 plt.show()
+
+
+"""decompose, trend"""
