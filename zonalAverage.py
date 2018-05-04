a64152ff58e8a03f2746581a23819657dde00da3
jingchaoyy <jcyangt@gmail.com>
2018-05-04 13:39:26 -0400

    subgroup latitude values

--- zonalAverage.py
+++ zonalAverage.py
@@ -8,42 +8,47 @@ import numpy as np
 import pandas as pd
 
 earthquakeLat = dataPrepro.earthquake['LATITUDE']
+earthquakeLatNotNan = earthquakeLat[pd.notnull(earthquakeLat)]
+# print(len(earthquakeLatNotNan))
+
 tsuLat = dataPrepro.tsu['LATITUDE']
+tsuLatNotNan = tsuLat[pd.notnull(tsuLat)]
+# print(len(tsuLatNotNan))
 
-latsInterval = np.arange(-90, 100, 10)
 
+latsInterval = np.arange(-90, 100, 10)  # list for coordinate groups
 
 def contEventByAtt(att):
-    attRanges, eventObjs = [], []
+    eventObjs = []
     for i in att:
-        print(i)
-        if i is not None:
-            if len(attRanges) > 0:
-                for j in attRanges:
-                    if i in range(j[0], j[1]):
-                        for k in eventObjs:
-                            if j == k.att:
-                                k.contEvent()
-                else:
-                    for z in range(len(latsInterval) - 1):
-                        if float(i) * 1000 in range(latsInterval[z] * 1000, latsInterval[z + 1] * 1000):
-                            attRanges.append((latsInterval[z], latsInterval[z + 1]))
-                            e = dataPrepro.EventObj()
-                            e.att = (latsInterval[z] / latsInterval[z + 1])
-                            e.contEvent()
-                            eventObjs.append(e)
-            else:
-                for y in range(len(latsInterval) - 1):
-                    if float(i) * 1000 in range(latsInterval[y] * 1000, latsInterval[y + 1] * 1000):
-                        attRanges.append((latsInterval[y], latsInterval[y + 1]))
+        flag = False  # set a flag to check if i satisfy one of objects in the eventObjs list
+        if len(eventObjs) > 0:
+            for j in eventObjs:
+                # for loop, checking if i satisfy one of objects in the eventObjs list
+                if int(float(i)*1000) in range(j.att[0]*1000, j.att[1]*1000):
+                    j.contEvent()
+                    flag = True  # when i satisfies one of objects in the eventObjs list, set the flag to true
+
+            if flag == False:  # create new object and count if i do not satisfy any existing eventObj
+                for z in range(len(latsInterval) - 1):
+                    if int(float(i) * 1000) in range(latsInterval[z] * 1000, latsInterval[z + 1] * 1000):
                         e = dataPrepro.EventObj()
-                        e.att = (latsInterval[y] / latsInterval[y + 1])
+                        e.att = ([latsInterval[z], latsInterval[z + 1]])
                         e.contEvent()
                         eventObjs.append(e)
+        else:  # if no event object created in the eventObjectList, create one
+            for y in range(len(latsInterval) - 1):
+                if int(float(i) * 1000) in range(latsInterval[y] * 1000, latsInterval[y + 1] * 1000):
+                    e = dataPrepro.EventObj()
+                    e.att = ([latsInterval[y], latsInterval[y + 1]])
+                    e.contEvent()
+                    eventObjs.append(e)
 
     return eventObjs
 
 
-test = contEventByAtt(earthquakeLat)
-# for t in test:
-#     print(t.att, t.cont)
+# earthquakeGroups = contEventByAtt(earthquakeLatNotNan)
+tsuGroups = contEventByAtt(tsuLatNotNan)
+total = 0
+
+
