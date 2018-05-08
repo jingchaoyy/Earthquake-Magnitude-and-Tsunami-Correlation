edd1afb85a4ff644e19b3076fa203d19c4d252b6
jingchaoyy <jcyangt@gmail.com>
2018-05-08 13:13:32 -0400

    adding plot

--- zonalAverage.py
+++ zonalAverage.py
@@ -6,6 +6,7 @@ Created on 5/2/18
 import dataPrepro
 import numpy as np
 import pandas as pd
+from matplotlib import pyplot as plt
 
 earthquakeLat = dataPrepro.earthquake['LATITUDE']
 earthquakeLatNotNan = earthquakeLat[pd.notnull(earthquakeLat)]
@@ -21,3 +22,34 @@ latsInterval = np.arange(-90, 100, 10)  # list for coordinate groups, interval f
 
 earthquakeGroups = dataPrepro.contEventByAttWithInterval(latsInterval,earthquakeLatNotNan)
 tsuGroups = dataPrepro.contEventByAttWithInterval(latsInterval,tsuLatNotNan)
+
+'''Zonal Avg. Plot
+Plotting Zonal Average, cont number of events
+that happened in a specific latitude zone
+'''
+
+earthquakeTup = dataPrepro.getTup(earthquakeGroups)
+tsuTup = dataPrepro.getTup(tsuGroups)
+
+# sort the event tuple list by latitude
+sortedearthquakeTup = sorted(earthquakeTup, key=lambda t: t[0])
+sortTsuTup = sorted(tsuTup, key=lambda t: t[0])
+
+eX, eY = [], []
+for et in sortedearthquakeTup:
+    eX.append(et[0])
+    eY.append(et[1])
+
+tX, tY = [], []
+for tt in sortTsuTup:
+    tX.append(tt[0])
+    tY.append(tt[1])
+
+fig, ax = plt.subplots()
+ax.plot(eX, eY, 'r', label="Earthquake Count")
+ax.plot(tX, tY, 'b', label="Tsunami Count")
+legend = ax.legend(loc='upper left', shadow=True, fontsize='large')
+plt.title('Zonal Average')
+plt.xlabel('Lats')
+plt.ylabel('Event Counts')
+plt.show()
