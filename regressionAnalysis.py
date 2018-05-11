d36ae7d3c2345c32a89dbf6d91f0e4a00375f465
jingchaoyy <jcyangt@gmail.com>
2018-05-11 11:30:30 -0400

    minor updates

--- regressionAnalysis.py
+++ regressionAnalysis.py
@@ -12,27 +12,28 @@ import dataFilter
 import correlation
 
 """Regression model regarding simple time series"""
-# model = sm.OLS(dataPrepro.equakeList, dataPrepro.tsuList).fit()
-# sumy = model.summary()
-# print(sumy)
-#
-# plt.scatter(dataPrepro.equakeList, dataPrepro.tsuList)
-# plt.xlabel("Earthquake")
-# plt.ylabel("Tsu")
-#
-# npEquake = np.array(dataPrepro.equakeList)
-# lr = LinearRegression()
-# lr.fit(npEquake.reshape(-1, 1), dataPrepro.tsuList)
-#
-# y_pred = lr.predict(npEquake.reshape(-1, 1))
-# plt.plot(npEquake, y_pred, color='red')
-#
-# plt.show()
+model = sm.OLS(dataPrepro.equakeList, dataPrepro.tsuList).fit()
+sumy = model.summary()
+print(sumy)
+
+plt.scatter(dataPrepro.equakeList, dataPrepro.tsuList)
+plt.xlabel("Earthquake Counts by Year")
+plt.ylabel("Tsu Counts by Year")
+plt.title("Linear Regression")
+
+npEquake = np.array(dataPrepro.equakeList)
+lr = LinearRegression()
+lr.fit(npEquake.reshape(-1, 1), dataPrepro.tsuList)
+
+y_pred = lr.predict(npEquake.reshape(-1, 1))
+plt.plot(npEquake, y_pred, color='red')
+
+plt.show()
 
 
-# """Regression model regarding simple tsunami event counts
-# by earthquake surface magnitude and focal depth
-# """
+"""Regression model regarding simple tsunami event counts
+by earthquake surface magnitude and focal depth
+"""
 # plt.subplot(211)
 # plt.scatter(correlation.msX, correlation.msY)
 # plt.title("Linear Regression")
@@ -59,6 +60,10 @@ import correlation
 # y_pred2 = lr2.predict(npFD.reshape(-1, 1))
 # plt.plot(npFD, y_pred2, color='red')
 #
+# fdConstant = sm.add_constant(npFD)
+# model2 = sm.OLS(correlation.fdY, fdConstant).fit()
+# sumy2 = model2.summary()
+# print(sumy2)
 #
 #
 # plt.show()
@@ -67,45 +72,45 @@ import correlation
 """Regression model regarding tsunami MWH Avg
 by earthquake surface magnitude and focal depth
 """
-plt.subplot(211)
-plt.scatter(correlation.ms_mwhX, correlation.ms_mwhY)
-plt.title("Linear Regression")
-plt.xlabel("Surface Magnitude")
-plt.ylabel("Max Water Height Avg (m)")
-
-npMS = np.array(correlation.ms_mwhX)
-# print OLS model for linear regression
-smConstant = sm.add_constant(npMS)
-model = sm.OLS(correlation.ms_mwhY, smConstant).fit()
-sumy = model.summary()
-print(sumy)
-
-lr = LinearRegression()
-lr.fit(npMS.reshape(-1, 1), correlation.ms_mwhY)
-
-y_pred = lr.predict(npMS.reshape(-1, 1))
-plt.plot(npMS, y_pred, color='red')
-
-###################################
-plt.subplot(212)
-plt.scatter(correlation.fd_mwhX, correlation.fd_mwhY)
-plt.xlabel("Forcal Depth (km)")
-plt.ylabel("Max Water Height Avg (m)")
-
-npFD = np.array(correlation.fd_mwhX)
-# print OLS model for linear regression
-fdConstant = sm.add_constant(npFD)
-model2 = sm.OLS(correlation.fd_mwhY, fdConstant).fit()
-sumy2 = model2.summary()
-print(sumy2)
-
-lr2 = LinearRegression()
-lr2.fit(npFD.reshape(-1, 1), correlation.fd_mwhY)
-
-y_pred2 = lr2.predict(npFD.reshape(-1, 1))
-plt.plot(npFD, y_pred2, color='red')
-
-plt.show()
+# plt.subplot(211)
+# plt.scatter(correlation.ms_mwhX, correlation.ms_mwhY)
+# plt.title("Linear Regression")
+# plt.xlabel("Surface Magnitude")
+# plt.ylabel("Max Water Height Avg (m)")
+#
+# npMS = np.array(correlation.ms_mwhX)
+# # print OLS model for linear regression
+# smConstant = sm.add_constant(npMS)
+# model = sm.OLS(correlation.ms_mwhY, smConstant).fit()
+# sumy = model.summary()
+# print(sumy)
+#
+# lr = LinearRegression()
+# lr.fit(npMS.reshape(-1, 1), correlation.ms_mwhY)
+#
+# y_pred = lr.predict(npMS.reshape(-1, 1))
+# plt.plot(npMS, y_pred, color='red')
+#
+# ###################################
+# plt.subplot(212)
+# plt.scatter(correlation.fd_mwhX, correlation.fd_mwhY)
+# plt.xlabel("Forcal Depth (km)")
+# plt.ylabel("Max Water Height Avg (m)")
+#
+# npFD = np.array(correlation.fd_mwhX)
+# # print OLS model for linear regression
+# fdConstant = sm.add_constant(npFD)
+# model2 = sm.OLS(correlation.fd_mwhY, fdConstant).fit()
+# sumy2 = model2.summary()
+# print(sumy2)
+#
+# lr2 = LinearRegression()
+# lr2.fit(npFD.reshape(-1, 1), correlation.fd_mwhY)
+#
+# y_pred2 = lr2.predict(npFD.reshape(-1, 1))
+# plt.plot(npFD, y_pred2, color='red')
+#
+# plt.show()
 
 """Regression model regarding earthquakes that cause tsunamis
 Model for earthquake surface magnitude earthquake forcal depth 
